c5d8ea7a61cb5ce3188001b9dea97f33bfca1fde
Young Jin <likeRudin@gmail.com>
2023-10-20 12:41:00 +0900

    REFACTORING: Component-Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Button from "./component/button";
 
 const Home: NextPage = () => {
   return (
@@ -72,22 +73,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
-      <button className="fixed hover:bg-orange-700 transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-600 rounded-full p-4 text-white font-medium">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-          />
-        </svg>
-      </button>
+      <Button tooltip="New MeetUp" />
     </div>
   );
 };
