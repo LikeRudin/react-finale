6e3a0fbfa28f97b2eb9e90c2aeec99c6f50bb716
Young Jin <likeRudin@gmail.com>
2023-10-20 21:48:10 +0900

    Hook: useMutation

---
+++ libs/client/useMutation.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useSWRMutation from "swr/mutation";
+
+type UseMutationState<T> =
+  | { fetchState: "ok"; responseData: T }
+  | { fetchState: "fail"; error: object }
+  | { fetchState: "loading" };
+
+type useMetationResult<T> = [(data: any) => void, UseMutationState<T>];
+
+const useMutation = <T = any>(url: string): useMetationResult<T> => {
+  const [state, setState] = useState<UseMutationState<T>>({
+    fetchState: "loading",
+  });
+
+  const { trigger } = useSWRMutation(url, async (requestData) => {
+    setState({ fetchState: "loading" });
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setState({ fetchState: "ok", responseData: data });
+      })
+      .catch((error) => {
+        setState({ fetchState: "fail", error });
+      });
+  });
+  return [trigger, state];
+};
+
+export default useMutation;
