0e7ebf2355129c6af81dcee3ff6eaa5714e03592
Young Jin <likeRudin@gmail.com>
2023-10-21 16:42:37 +0900

    add API Route for login and sign-in

---
+++ constants/apiroutes.ts
@@ -0,0 +1,6 @@
+export const enum APIROUTE {
+  ENTER_LOGIN = "/api/users/existence",
+  ENTER_PASSWORD = "/api/users/enter",
+  ENTER_SIGNIN = "/api/users/available-test",
+  ENTER_CREATION = "/api/users",
+}
