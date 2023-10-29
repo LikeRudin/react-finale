a6fbd939d2ab8225eaecbab47b08a382c36275f1
Young Jin <likeRudin@gmail.com>
2023-10-29 20:32:23 +0900

    REFACTORING: useUser give different value per page

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,6 +2,8 @@ import useSWR from "swr";
 import { APIROUTE } from "@/constants/apiroutes";
 import type { KeyedMutator } from "swr";
 
+type PageInfo = "MEETUP" | "PROFILE" | "CHAT" | "LIVE" | "";
+
 type UseUserState<T> =
   | { status: "ok"; userData: T; mutate: KeyedMutator<any> }
   | { status: "fail"; error: object | string }
@@ -10,20 +12,22 @@ type UseUserState<T> =
 
 type UserSWR<T> = UseUserState<T>;
 
-const useUser = <T = any>(): UserSWR<T> => {
-  const { data, error, mutate } = useSWR(APIROUTE.ANY_USE_USER, (url: string) =>
-    fetch(url)
-      .then((response) => response.json())
-      .then((parsed) => {
-        const { ok, data, error } = parsed;
-        const result = ok
-          ? { status: "ok", userData: data }
-          : { status: "fail", error: error as string };
-        return result;
-      })
-      .catch((error) => {
-        return error;
-      })
+const useUser = <T>(page: PageInfo = ""): UseUserState<T> => {
+  const { data, error, mutate } = useSWR(
+    APIROUTE.ANY_USE_USER(page.toLowerCase()),
+    (url: string) =>
+      fetch(url)
+        .then((response) => response.json())
+        .then((parsed) => {
+          const { ok, data, error } = parsed;
+          const result = ok
+            ? { status: "ok", userData: data }
+            : { status: "fail", error: error as string };
+          return result;
+        })
+        .catch((error) => {
+          return error;
+        })
   );
   const user: UseUserState<T> = data
     ? { ...data, mutate }
