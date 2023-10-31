31e104326c6fbed832b0fdb7d90d92c410737463
Young Jin <likeRudin@gmail.com>
2023-10-31 13:59:34 +0900

    input/text area - handle warning

--- pages/components/input.tsx
+++ pages/components/input.tsx
@@ -1,4 +1,5 @@
 import type { UseFormRegisterReturn, UseFormSetValue } from "react-hook-form";
+import React, { useEffect } from "react";
 
 interface InputProps {
   label: string;
@@ -7,8 +8,8 @@ interface InputProps {
   register: UseFormRegisterReturn;
   required: boolean;
   placeholder: string;
-  setValue: UseFormSetValue<Text>;
-  value: string;
+  setValue: UseFormSetValue<any>;
+  value?: string;
 }
 
 const Input = ({
@@ -21,15 +22,17 @@ const Input = ({
   setValue,
   value,
 }: InputProps) => {
-  if (setValue) {
-    setValue(name, value);
-  }
-  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  useEffect(() => {
+    if (value) {
+      setValue(name, value);
+    }
+  });
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(name, e.target.value);
   };
   return (
     <>
-      <label htmlFor={name} className="text-sm font-medium text-gray-[650]">
+      <label htmlFor={name} className='text-sm font-medium text-gray-[650]'>
         {label}
       </label>
       <div>
@@ -38,7 +41,7 @@ const Input = ({
           type={type}
           required={required}
           {...register}
-          className="appearance-none w-full px-2 py-2 border border-orange-900 border-opacity-30 rounded-md shadow-sm placeholder-orange-800 focus:outline-none focus:ring-orange-400 focus:border-orange-800 placeholder:text-gray-400 text-gray-800"
+          className='appearance-none w-full px-2 py-2 border border-orange-900 border-opacity-30 rounded-md shadow-sm placeholder-orange-800 focus:outline-none focus:ring-orange-400 focus:border-orange-800 placeholder:text-gray-400 text-gray-800'
           placeholder={placeholder}
           onChange={onChange}
         />
--- pages/components/textarea.tsx
+++ pages/components/textarea.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { UseFormRegisterReturn, UseFormSetValue } from "react-hook-form";
 
 interface TextAreaProps {
   label: string;
   name: string;
   register: UseFormRegisterReturn;
-  setValue?: UseFormSetValue<Text>;
+  setValue: UseFormSetValue<any>;
   required: boolean;
   placeholder?: string;
   value: string;
@@ -20,30 +20,31 @@ const TextArea = ({
   setValue,
   value,
 }: TextAreaProps) => {
-  if (setValue) {
-    setValue(name, value);
-  }
+  useEffect(() => {
+    if (value) {
+      setValue(name, value);
+    }
+  });
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(name, e.target.value);
   };
 
   return (
-    <div className="mt-2 h-full">
-      <label htmlFor={name} className="mb-1 block text-sm font-medium ">
+    <div className='mt-2 h-full'>
+      <label htmlFor={name} className='mb-1 block text-sm font-medium '>
         {label}
       </label>
       <div>
         <textarea
           id={name}
           rows={4}
-          className="text-gray-800 mt-1 shaodw-sm w-full focus:ring-orange-800 focus:ring-2 focus:ring-offset-1  rounded-md  border-gray-400 focus:border-transparent"
+          className='text-gray-800 mt-1 shaodw-sm w-full focus:ring-orange-800 focus:ring-2 focus:ring-offset-1  rounded-md  border-gray-400 focus:border-transparent'
           required={required}
           placeholder={placeholder}
           {...register}
           onChange={onChange}
-        >
-          {value}
-        </textarea>
+          defaultValue={value}
+        ></textarea>
       </div>
     </div>
   );
