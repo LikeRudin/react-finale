580b48c299e7fe2384f60a329bd68e1d78c95fc9
Young Jin <likeRudin@gmail.com>
2023-11-12 17:50:29 +0900

    Middleware: if logged-in, cannot access to enterpage

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,12 @@ export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
 
   const path = new URL(req.url, process.env.BASE_URL).pathname;
+  const cookies = req.cookies.getAll();
 
-  if (!req.cookies.getAll().length && !path.includes("enter")) {
+  if (!cookies.length && !path.includes("enter")) {
     return NextResponse.redirect(new URL(ROUTE_PATH.ENTER, req.url));
+  } else if (cookies.length && path.includes("enter")) {
+    return NextResponse.redirect(new URL(ROUTE_PATH.INDEX, req.url));
   }
 
   return res;
